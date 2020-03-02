1241d3722cf5e019aedcb91ca818fd38bdc8ad25
kjkim <apio1130@daum.net>
2020-03-02 14:39:04 +0900

    불필요 파일 제거

--- todo-list-common/src/test/java/com/todolist/common/repository/GroupsRepositoryTest.java
+++ todo-list-common/src/test/java/com/todolist/common/repository/GroupsRepositoryTest.java
@@ -36,9 +36,9 @@ class GroupsRepositoryTest {
 
     @BeforeEach
     void setUp() {
-        groupsRepository.save(Groups.builder().title("할일").delYn("N").build());
+        Groups group = groupsRepository.save(Groups.builder().title("할일").delYn("N").build());
         IntStream.range(1, 11).forEach(i -> taskRepository.save(Task.builder().title("To-do #" + i)
-                .cmplYn("N").delYn("N").build()));
+                .cmplYn("N").delYn("N").groups(group).build()));
     }
 
     @DisplayName("Groups 조회 테스트")
@@ -63,14 +63,16 @@ class GroupsRepositoryTest {
     @DisplayName("Groups 수정 테스트")
     @Test
     void update() {
-        Groups groups = groupsRepository.findById(1L).orElse(new Groups());
-        groups.update(Groups.builder()
-                .title("오늘 할일")
-                .delYn(groups.getDelYn())
-                .build());
-        groupsRepository.save(groups);
-
         Groups findGroups = groupsRepository.findById(1L).orElse(new Groups());
+        log.debug("find group info : {}", findGroups);
+        findGroups.update(Groups.builder()
+                .title("오늘 할일")
+                .delYn(findGroups.getDelYn())
+                .build());
+        groupsRepository.save(findGroups);
+
+        Groups updatedGroups = groupsRepository.findById(findGroups.getGroupNo()).orElse(new Groups());
+        log.debug("updated Groups info : {}", updatedGroups);
         assertThat(findGroups.getTitle(), is("오늘 할일"));
     }
 
